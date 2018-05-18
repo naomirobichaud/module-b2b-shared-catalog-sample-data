6c6e1db27db6a37e68566bfdc326eae83c4a9e80
Jeff Britts <jbritts@magento.com>
2018-05-18 16:14:58 -0400

    change for splitting module

--- Setup/Installer.php
+++ Setup/Installer.php
@@ -65,9 +65,9 @@ class Installer implements Setup\SampleData\InstallerInterface
      * {@inheritdoc}
      */
     public function install()
-    {   $this->catalogSetup->install();
+    {   //$this->catalogSetup->install();
         $this->relatedProducts->install(['MagentoEse_B2BSharedCatalogSampleData::fixtures/related_products.csv']);
-        $this->sharedCatalogConfig->install();
+        //$this->sharedCatalogConfig->install();
         $this->preferredProducts->install(['MagentoEse_B2BSharedCatalogSampleData::fixtures/preferredproducts.csv']);
         $this->tierPricing->install([
             'MagentoEse_B2BSharedCatalogSampleData::fixtures/legrand_tier_pricing.csv',
